03f105abdecc577929be62e4041bce487eaf6108
Turtley12 <76929530+Turtley12@users.noreply.github.com>
2022-01-24 20:03:59 -0800

    Add install scripts. can be used to install but mainly intended for an easy way to add pluto to itself.

---
+++ scripts/install.sh
@@ -0,0 +1,4 @@
+echo Make sure you have a working Go installation. Tested on Go 1.17, but might work on earlier versions
+go build .
+cp ./pluto /usr/local/bin/
+echo Built pluto and installed to /usr/local/bin/pluto
