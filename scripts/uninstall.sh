f2b98b2b7a19238e19f54b4b3de1002061d4ce3c
Turtley12 <76929530+Turtley12@users.noreply.github.com>
2022-01-24 20:16:28 -0800

    Fix uninstall

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,4 +1,4 @@
-rm /usr/local/bin/pluto
+sudo rm /usr/local/bin/pluto
 echo Removed pluto from /usr/local/bin/pluto.
 echo Programs that have been installed using pluto have not been removed.
 echo Source code that pluto downloaded when installing programs is still in $HOME/.pluto
